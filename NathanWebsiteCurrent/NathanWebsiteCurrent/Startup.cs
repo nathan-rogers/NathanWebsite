7ccc7810ac9a01c1f410e04774b21e5cae4488ab
nathan-rogers <nathan.rogers.d@gmail.com>
2015-05-15 14:14:15 -0600

    Initial Commit

---
+++ NathanWebsiteCurrent/NathanWebsiteCurrent/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NathanWebsiteCurrent.Startup))]
+namespace NathanWebsiteCurrent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
