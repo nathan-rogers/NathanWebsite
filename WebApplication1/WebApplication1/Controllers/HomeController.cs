11f4788dc0fd9edfb846ce0fae392c9984d446e3
nathan-rogers <nathan.rogers.d@gmail.com>
2015-05-17 15:46:26 -0600

    IMG content and external media inks added

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult About()
+        {
+            ViewBag.Active = "about";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult About(HttpPostedFileBase file)
+        {
+            try
+            {
+                var filename = Guid.NewGuid().ToString().Substring(0,5) + "_" + Path.GetFileName(file.FileName);
+                var filenamePath = Path.Combine(Server.MapPath("~/Content/Uploads"), filename);
+                file.SaveAs(filenamePath);//saves file
+                ViewBag.Message = "Good job";
+            }
+            catch (Exception e)
+            {
+                ViewBag.Message = "Bad Job" + e.Message;
+                
+            }
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
